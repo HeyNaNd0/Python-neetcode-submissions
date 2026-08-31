430d51f0bb90fb459a88570dc078cae2401c1724
Eric Fernandez <22282823+HeyNaNd0@users.noreply.github.com>
2026-08-31 21:48:51 +0000

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,15 @@
+
+variable = 10
+print(type(variable))
+
+variable = 1.6 
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "lol"
+print(type(variable))
+
+variable = [1,2,3]
+print(type(variable))
