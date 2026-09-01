420cc7e0b618598788400fac678e82b6c5d3e070
Eric Fernandez <22282823+HeyNaNd0@users.noreply.github.com>
2026-09-01 08:59:50 +0000

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,11 @@
+a, b, c = 2, 8, 5
+
+floor_division = (a * b) // c
+modulus = (a* b) % c
+raising_a = a ** b
+raising_b = b ** c
+
+print (floor_division)
+print (modulus)
+print (raising_a)
+print (raising_b)
